66bce9f4a4846ceb37aad0fec8ab6c9190a1cc0b
Jéssica Bernardi Petersen <jessica041ster@gmail.com>
2019-12-23 12:12:15 -0300

    classes

---
+++ LibrarySystem/app/src/main/java/com/example/librarysystem/Livro.java
@@ -0,0 +1,74 @@
+package com.example.librarysystem;
+
+import android.graphics.drawable.Icon;
+
+public class Livro {
+
+    private String titulo, autor, editora, ano, localizacao;
+    private int quantidadeTotal, quantidadeDisponivel;
+    private Icon imagem;
+
+    public Icon getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(Icon imagem) {
+        this.imagem = imagem;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    public int getQuantidadeTotal() {
+        return quantidadeTotal;
+    }
+
+    public void setQuantidadeTotal(int quantidadeTotal) {
+        this.quantidadeTotal = quantidadeTotal;
+    }
+
+    public int getQuantidadeDisponivel() {
+        return quantidadeDisponivel;
+    }
+
+    public void setQuantidadeDisponivel(int quantidadeDisponivel) {
+        this.quantidadeDisponivel = quantidadeDisponivel;
+    }
+}
---
+++ LibrarySystem/app/src/main/java/com/example/librarysystem/LivroAdapter.java
@@ -0,0 +1,66 @@
+package com.example.librarysystem;
+
+import android.content.Context;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class LivroAdapter extends RecyclerView.Adapter<LivroAdapter.LivroHolder> {
+    private Context context;
+    private ArrayList<Livro> livros;
+
+    public LivroAdapter(Context context, ArrayList<Livro> livros){
+        this.context = context;
+        this.livros = livros;
+    }
+
+    @NonNull
+    @Override
+    public LivroHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.book_layout, parent, false);
+        return new LivroHolder(v);
+    }
+
+    public Livro getLivro(int position){
+        return livros.get(position);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onBindViewHolder(@NonNull LivroHolder holder, int position) {
+        final Livro livro = getLivro(position);
+        if(livro != null){
+            holder.titulo.setText(livro.getTitulo());
+            holder.imagem.setImageIcon(livro.getImagem());
+            holder.autor.setText(livro.getAutor());
+            holder.ano.setText(livro.getAno());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return livros.size();
+    }
+
+
+    class LivroHolder extends RecyclerView.ViewHolder{
+        TextView titulo, autor, ano;
+        ImageView imagem;
+        public LivroHolder(View itemView){
+            super(itemView);
+            titulo = itemView.findViewById(R.id.titulo_book_list);
+            autor = itemView.findViewById(R.id.autor_book_list);
+            ano = itemView.findViewById(R.id.ano_book_list);
+            imagem = itemView.findViewById(R.id.imagem_book_list);
+        }
+    }
+}
