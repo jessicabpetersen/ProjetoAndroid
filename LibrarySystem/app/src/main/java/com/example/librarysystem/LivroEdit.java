08db29b59e31a0fffa5ab215c9aab7e95441f3e2
Jéssica Bernardi Petersen <jessica041ster@gmail.com>
2019-12-24 09:37:27 -0300

    .

---
+++ LibrarySystem/app/src/main/java/com/example/librarysystem/LivroEdit.java
@@ -0,0 +1,14 @@
+package com.example.librarysystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LivroEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_livro_edit);
+    }
+}
